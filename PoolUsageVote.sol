b20360b792bc112417e8c1e5d7e93eb6848bf328
votkon <60887038+votkon@users.noreply.github.com>
2023-02-14 14:07:37 +0100

    Added results interaction

--- PoolUsageVote.sol
+++ PoolUsageVote.sol
@@ -1,22 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/access/Ownable.sol";
-//import "https://github.com/ipv4dao/root_tokens/blob/main/ipv4dao_root_tokens.sol";
+//import "./ipv4dao_root_tokens.abi.json";
+interface RootTokensContract {
+        function totalSupply() external view returns (uint256);
+        function ownerOf(uint256 tokenId) external view returns (address owner);
+        function balanceOf(address owner) external view returns (uint256 balance);
+}
+
 contract PoolUsageVote is Ownable {
+    RootTokensContract rootTokens = RootTokensContract(address(0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692));
+
     struct Votes {
         uint8 grow;
         uint8 help;
         uint8 daouse;
         uint8 liquidate;
     }
+    struct Results {
+        uint256 grow;
+        uint256 help;
+        uint256 daouse;
+        uint256 liquidate;
+        uint256 timestamp;
+    } 
+    Results public results;
     mapping(address => Votes) public poolVotes;
 
     function vote(Votes memory _newVote) public {
-        require(_newVote.grow+_newVote.help+_newVote.daouse+_newVote.liquidate==100, "The sum should be equal to 100");
-        poolVotes[msg.sender].grow=_newVote.grow;
-        poolVotes[msg.sender].help=_newVote.help;
-        poolVotes[msg.sender].daouse=_newVote.daouse;
-        poolVotes[msg.sender].liquidate=_newVote.liquidate;
+        //Checking that the sum is 100
+        require(_newVote.grow+_newVote.help+_newVote.daouse+_newVote.liquidate==100, "The sum of all options should be equal to 100");
+        require(rootTokens.balanceOf(msg.sender)>0, "You should own at least one Root Token to vote");
+        poolVotes[msg.sender]=_newVote;
 
+  }
+  function refreshResults () public {
+      Results memory newResult;
+      newResult.grow=0;
+      newResult.help=0;
+      newResult.daouse=0;
+      newResult.liquidate=0;
+      uint256 totalSupply=rootTokens.totalSupply();
+      for (uint256 i=1; i <= totalSupply; i++) {
+        address curOwner;
+        curOwner = rootTokens.ownerOf(i);
+        newResult.grow += poolVotes[curOwner].grow;
+        newResult.help += poolVotes[curOwner].help;
+        newResult.daouse += poolVotes[curOwner].daouse;
+        newResult.liquidate += poolVotes[curOwner].liquidate;
+        }
+    newResult.timestamp=block.timestamp;
+    results=newResult;
   }
 }
